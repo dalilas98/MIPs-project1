a3a3c2f426099043480d5701fec943bcf67f669e
dalilas98 <44147704+dalilas98@users.noreply.github.com>
2018-10-19 14:28:35 -0400

    Add files via upload

--- mips1.asm
+++ mips1.asm
@@ -1,40 +1,47 @@
 .data #variables 
-	myID: .word     0002820530
+	myID: .word     02820530
 	
 
 .text
 	li $a0, '@'
 	li $v0, 11
 	syscall 
+	sub $v0, $v0, 10
+	li $a0, 0
+	syscall
 	lw $a0, myID
 	li $v0, 1
 	syscall
- addi $v0,$v0, 10
-    addi $a0,$a0, 5       # LF (10)
-    syscall
-    addi $a0,$a0, 83     # S(67)
-    syscall
-    addi $a0,$a0, 99     # c (111)
-    syscall
-    sub $a0,$a0, 111      # o (111)
-    syscall
-    sub $a0, $a0,116       # t (108)
-     syscall
-    sub $a0, $a0,116      # t (101)
-     syscall
-    sub $a0,$a0, 77       # , (44)
-    syscall
-    sub $a0, $a0,12       # [space] (32)
-    syscall
-    addi $a0,$a0, 68      # D (74)
-    syscall
-    addi $a0, $a0, 97 # a (97)
-    syscall
-    addi $a0, $a0, 108 # l (115)
-    syscall
-    sub $a0, $a0, 105  # i(109)
-    syscall
-    addi $a0, $a0, 108 # l(111)
-    syscall
-    sub $a0, $a0, 97   # a (110)
-    syscall
+	
+	li $v0, 11
+	li $a0, 0
+	addi $a0, $a0, 10
+	syscall
+	addi $a0, $a0, 73
+	syscall
+	addi $a0,$a0, 16     # c (99)
+    	syscall
+    	addi $a0,$a0, 12      # o (111)
+    	syscall
+    	addi $a0, $a0,5       # t (116)
+     	syscall
+    	sub $a0, $a0,0      # t (116)
+     	syscall
+    	sub $a0,$a0, 72      # , (44)
+    	syscall
+    	sub $a0, $a0,12       # [space] (32)
+    	syscall
+    	addi $a0,$a0, 36     # D (68)
+    	syscall
+    	addi $a0, $a0, 29 # a (97)
+    	syscall
+    	addi $a0, $a0, 11 # l (108)
+    	syscall
+    	sub $a0, $a0, 3  # i(105)
+    	syscall
+    	addi $a0, $a0, 3 # l(108)
+    	syscall
+    	sub $a0, $a0, 11   # a (97)
+    	syscall
+	
+    
